484e724c847acb89aaf16184d607e89768e57e07
chris <cryptoskillz@chriss-MacBook-Air.local>
2022-11-20 13:58:09 +0700

    first

--- functions/api/database.js
+++ functions/api/database.js
@@ -1,19 +1,11 @@
 export async function onRequest(context) { 
 
-    const {
-        request, // same as existing Worker API
-        env, // same as existing Worker API
-        params, // if filename includes [id] or [[path]]
-        waitUntil, // same as ctx.waitUntil in existing Worker API
-        next, // used for middleware or to fetch assets
-        data, // arbitrary space for passing data between middlewares
-    } = context;
 
 
   // Create a prepared statement with our query
   console.log(context.env)
   
-  const ps = context.env.buildingblocks.prepare('SELECT * from property'); 
+  const ps = context.env.database.prepare('SELECT * from property'); 
   const result = await ps.first();
   //console.log(data)
   return Response.json(result);
