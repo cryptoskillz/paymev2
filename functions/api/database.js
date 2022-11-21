2ff4fbaecea4216ccc19c172c1271d6a1fa9bd20
chris <cryptoskillz@chriss-MacBook-Air.local>
2022-11-21 23:04:57 +0700

    worker

--- functions/api/database.js
+++ functions/api/database.js
@@ -1,10 +1,11 @@
 export async function onRequest(context) { 
+    console.log(context.env)
   // Create a prepared statement with our query
-  //const ps = context.env.database.prepare('SELECT * from property'); 
-  //const result = await ps.first();
-  //console.log(data)
-  console.log(context.env)
-  //return Response.json(data)
+  const ps = context.env.DB.prepare('SELECT * from property'); 
+  const result = await ps.first();
+  console.log(result)
 
-  return Response.json(context.env.test3);
+  return Response.json(result)
+
+  //return Response.json(context.env.DB);
 }
