cac6d899e3f500cc579d9fe06dcc996d4efaf2c5
chris <cryptoskillz@chriss-MacBook-Air.local>
2022-12-08 14:01:47 +0700

    property token overrides

--- _includes/property-token-new.js
+++ _includes/property-token-new.js
@@ -14,7 +14,8 @@ whenDocumentReady(isReady = () => {
    let symbol = name.substring(0,3);
    document.getElementById('inp-name').value = name+'Token';
    document.getElementById('inp-totalSupply').value = dataItem.localCost;
-   document.getElementById('inp-contractSymbol').value = symbol
+   document.getElementById('inp-contractSymbol').value = symbol;
+   document.getElementById('inp-propertyId').value = dataItem.id;
    console.log(theEl)
 
 
