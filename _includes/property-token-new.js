dbe8ef9eee71b69bb509f1fe31f000725057f92e
chris <cryptoskillz@chriss-MacBook-Air.local>
2022-12-08 14:06:11 +0700

    clean up

--- _includes/property-token-new.js
+++ _includes/property-token-new.js
@@ -1,22 +1,35 @@
+/*
 
+    As this page interacts with the block chain it does a lot more than the standard table insert to we have to override it with its own 
+    new.js to stop the core insert from becoming to bloated. 
+
+
+*/
 //add a ready function
 let whenDocumentReady = (f) => {
     /in/.test(document.readyState) ? setTimeout('whenDocumentReady(' + f + ')', 9) : f()
 }
 
 whenDocumentReady(isReady = () => {
-            //show the body div
-        document.getElementById('showBody').classList.remove('d-none');
+    //show the body div
+    document.getElementById('showBody').classList.remove('d-none');
+    //get the current property
+    let dataItem = JSON.parse(window.localStorage.currentDataItem);
+    //clean up the name
+    let name = dataItem.name.replace(" ", "");
+    //get a symbol
+    let symbol = name.substring(0, 3);
+    //set the token name
+    document.getElementById('inp-name').value = name + 'Token';
+    //set the token supply
+    document.getElementById('inp-totalSupply').value = dataItem.localCost;\
+    //set the toekn symbol
+    document.getElementById('inp-contractSymbol').value = symbol;
+    //set the property id
+    document.getElementById('inp-propertyId').value = dataItem.id;
+    //get the deployed value
+    document.getElementById('inp-isDeployed').value = "0";
 
-   //console.log(window.localStorage.currentDataItem)
-   let dataItem = JSON.parse(window.localStorage.currentDataItem);
-   let name= dataItem.name.replace(" ","");
-   let symbol = name.substring(0,3);
-   document.getElementById('inp-name').value = name+'Token';
-   document.getElementById('inp-totalSupply').value = dataItem.localCost;
-   document.getElementById('inp-contractSymbol').value = symbol;
-   document.getElementById('inp-propertyId').value = dataItem.id;
-   console.log(theEl)
 
 
 });
