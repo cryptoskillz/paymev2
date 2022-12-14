ef49fa4a3840555e9edc52878b3f31358fb85001
chris <cryptoskillz@chriss-MacBook-Air.local>
2022-12-14 19:43:29 +0700

    dist

--- _includes/property-distributions-new.js
+++ _includes/property-distributions-new.js
@@ -68,6 +68,113 @@ const getAccounts = async () => {
     return account[0];
 }
 
+const updateValues = async (disResults, customValue = 0) => {
+
+
+    //get the balance from Metamask
+    let balance = await web3.eth.getBalance(currentAccountAddress);
+    //convert it to something useful
+    currentBalance = await web3.utils.fromWei(balance, 'ether');
+    //set the contract address
+    const addr = "0x2514895c72f50D8bd4B4F9b1110F0D6bD2c97526";
+    //set the price contract
+    const priceFeed = new web3.eth.Contract(aggregatorV3InterfaceABI, addr);
+    //get the latest price
+    let roundData = await priceFeed.methods.latestRoundData().call();
+    //get the decimals (didn't mention this in the docs anywhere did you)
+    let decimals = await priceFeed.methods.decimals().call();
+    //convert the stupid big number
+    let currentPrice = Number((roundData.answer.toString() / Math.pow(10, decimals)).toFixed(2));
+    //get the value of the crypto in USD
+    //note : make this the default currency from the settings
+    let cyrptoValue
+    if (customValue == 0)
+        cyrptoValue = currentPrice / 100 * currentBalance * 100
+    else
+        cyrptoValue = currentPrice / 100 * customValue * 100
+
+
+    //update the values
+    cryptoValue = Number(cyrptoValue.toFixed(2));
+    let cyrptoValuef = formatCurencyUSD(cryptoValue);
+    let totalCostsf = formatCurencyUSD(disResults.totalCosts);
+    let totalPaymentsf = formatCurencyUSD(disResults.totalPayments);
+    let totalDistf = formatCurencyUSD(disResults.totalDistributions);
+    let totalLeftf = formatCurencyUSD(disResults.totalLeft);
+    let perToLeave = document.getElementById('inp-perLeave').value;
+    let totalToPay
+    let totalToPayUSD
+    if (customValue == 0) {
+        totalToPay = currentBalance / 100 * perToLeave;
+        totalToPay = currentBalance - totalToPay
+        totalToPayUSD = cryptoValue / 100 * perToLeave
+        totalToPayUSD = cryptoValue - totalToPayUSD;
+    } else {
+        totalToPay = customValue / 100 * perToLeave;
+        totalToPay = customValue - totalToPay
+        totalToPayUSD = cryptoValue / 100 * perToLeave
+        totalToPayUSD = cryptoValue - totalToPayUSD;
+    }
+
+    if (customValue > currentBalance) {
+        updateIt = 0;
+        showAlert(`Custom amount cannot be greater than ${currentBalance}`, 2, 1)
+        updateValues(disResults)
+    } else {
+        let totalToPayUSDf = formatCurencyUSD(totalToPayUSD);
+        //console.log(totalToPay)
+        if (disResults.totalLeft > cryptoValue) {
+            showAlert("There is less funds in the account than the total owed so we are going to use the whole balance", 2, 0)
+        }
+        let table = `<table class="table">`
+        table = addTableRow("Current BNB price", formatCurencyUSD(currentPrice), table);
+        table = addTableRow("Token", `<a href="${disResults.token.blockExplorerUrl}" target="_blank">${disResults.token.name}</a>`, table);
+        table = addTableRow("Available BNB", `${currentBalance} (${cyrptoValuef})`, table);
+        table = addTableRow("Total Costs", totalCostsf, table);
+        table = addTableRow("Total Payments", totalPaymentsf, table);
+        table = addTableRow("Total Distributions", totalDistf, table);
+        table = addTableRow("Total Left", totalLeftf, table);
+        table = addTableRow("Total To Pay", ` ${totalToPay.toFixed(8)} (${totalToPayUSDf})`, table);
+        table = table + "</table>";
+        //console.log(table)
+        let theTable = document.getElementById("dist-table");
+        theTable.innerHTML = table;
+
+        table = $('#owners-table').DataTable();
+        table.clear();
+        for (var i = 0; i < disResults.owners.length; ++i) {
+            let theData = disResults.owners[i];
+            let per = theData.tokenAmount / disResults.token.totalSupply;
+            per = per * 100
+            per = Math.round(per);
+
+            //work out the amount of eth they getting
+            let ownerAmount = totalToPay / 100 * per;
+            ownerAmount = totalToPay - ownerAmount;
+
+            let ownerToPayUSD = totalToPayUSD / 100 * per
+            let ownerToPayUSDf = formatCurencyUSD(ownerToPayUSD);
+            //ownerToPayUSD = currentPrice - ownerToPayUSD;
+            disResults.owners[i].payAmount = ownerAmount;
+            disResults.owners[i].payAmountInternational = ownerToPayUSD;
+            //console.log(theData)
+
+            var rowNode = table
+                .row.add([theData.name, theData.email, theData.cryptoAddress,
+                    `${theData.tokenAmount} (${per}%)`, `${ownerAmount.toFixed(8)} (${ownerToPayUSDf})`
+                ])
+                .draw()
+
+        }
+        table.columns.adjust();
+
+        if (customValue == 0)
+            document.getElementById('inp-customAmount').value = totalToPay.toFixed(8);
+
+        window.localStorage.distributions = JSON.stringify(disResults);
+    }
+}
+
 
 //check if we are connected to metamask and if not then show the connect meta button
 const isConnected = async (disResults) => {
@@ -91,88 +198,10 @@ const isConnected = async (disResults) => {
             //set connectiont to false
             conn = false;
         } else {
-            //get the balance from Metamask
-            let balance = await web3.eth.getBalance(currentAccountAddress);
-            //convert it to something useful
-            currentBalance = await web3.utils.fromWei(balance, 'ether');
-            //set the contract address
-            const addr = "0x2514895c72f50D8bd4B4F9b1110F0D6bD2c97526";
-            //set the price contract
-            const priceFeed = new web3.eth.Contract(aggregatorV3InterfaceABI, addr);
-            //get the latest price
-            let roundData = await priceFeed.methods.latestRoundData().call();
-            //get the decimals (didn't mention this in the docs anywhere did you)
-            let decimals = await priceFeed.methods.decimals().call();
-            //convert the stupid big number
-            let currentPrice = Number((roundData.answer.toString() / Math.pow(10, decimals)).toFixed(2));
-            //get the value of the crypto in USD
-            //note : make this the default currency from the settings
-            let cyrptoValue = currentPrice / 100 * currentBalance * 100
-            //update the values
-            cryptoValue = Number(cyrptoValue.toFixed(2));
-            let cyrptoValuef = formatCurencyUSD(cryptoValue);
-            let totalCostsf = formatCurencyUSD(disResults.totalCosts);
-            let totalPaymentsf = formatCurencyUSD(disResults.totalPayments);
-            let totalDistf = formatCurencyUSD(disResults.totalDistributions);
-            let totalLeftf = formatCurencyUSD(disResults.totalLeft);
-            let perToLeave = document.getElementById('inp-perLeave').value;
-            let totalToPay = currentBalance / 100 * perToLeave;
-            totalToPay = currentBalance - totalToPay
-            let totalToPayUSD = cryptoValue / 100 * perToLeave
-            totalToPayUSD = cryptoValue - totalToPayUSD;
-
-            let totalToPayUSDf = formatCurencyUSD(totalToPayUSD);
-            //console.log(totalToPay)
-            if (disResults.totalLeft > cryptoValue) {
-                showAlert("There is less funds in the account than the total owed so we are going to use the whole balance", 2, 0)
-            }
-            let table = `<table class="table">`
-            table = addTableRow("Current BNB price", formatCurencyUSD(currentPrice), table);
-            table = addTableRow("Token", `<a href="${disResults.token.blockExplorerUrl}" target="_blank">${disResults.token.name}</a>`, table);
-            table = addTableRow("Available BNB", `${currentBalance} (${cyrptoValuef})`, table);
-            table = addTableRow("Total Costs", totalCostsf, table);
-            table = addTableRow("Total Payments", totalPaymentsf, table);
-            table = addTableRow("Total Distributions", totalDistf, table);
-            table = addTableRow("Total Left", totalLeftf, table);
-            table = addTableRow("Total To Pay", ` ${totalToPay.toFixed(8)} (${totalToPayUSDf})`, table);
-            table = table + "</table>";
-            //console.log(table)
-            let theTable = document.getElementById("dist-table");
-            theTable.innerHTML = table;
-
-
-
-
-            table = $('#owners-table').DataTable();
-            for (var i = 0; i < disResults.owners.length; ++i) {
-                let theData = disResults.owners[i];
-                let per = theData.tokenAmount / disResults.token.totalSupply;
-                per = per * 100
-                per = Math.round(per);
-
-                //work out the amount of eth they getting
-                let ownerAmount = totalToPay / 100 * per;
-                ownerAmount = totalToPay - ownerAmount;
-
-                let ownerToPayUSD = totalToPayUSD / 100 * per
-                let ownerToPayUSDf = formatCurencyUSD(ownerToPayUSD);
-                //ownerToPayUSD = currentPrice - ownerToPayUSD;
-                disResults.owners[i].payAmount = ownerAmount;
-                disResults.owners[i].payAmountInternational = ownerToPayUSD;
-                //console.log(theData)
-                var rowNode = table
-                    .row.add([theData.name, theData.email, theData.cryptoAddress,
-                        `${theData.tokenAmount} (${per}%)`, `${ownerAmount.toFixed(8)} (${ownerToPayUSDf})`
-                    ])
-                    .draw()
-
-            }
-            table.columns.adjust();
-
-            window.localStorage.distributions = JSON.stringify(disResults);
+            updateValues(disResults)
             document.getElementById('showBody').classList.remove('d-none');
             document.getElementById("spinner").classList.add("d-none");
-            document.getElementById('btn-pay').disabled = false;
+            //document.getElementById('btn-pay').disabled = false;
         }
         //set connection to true
         conn = true;
@@ -207,15 +236,36 @@ const ethRequestSend = async (toAddress, fromAddress, theValue) => {
             method: 'eth_sendTransaction',
             params: [transactionParameters],
         });
-        return {"status":"ok","hash":`${txHash}`};
+        return { "status": "ok", "hash": `${txHash}` };
     } catch (e) {
         //show the error (usually user hitting the reject button on metamask)
-        return {"status":"error","hash":""};
-    
+        return { "status": "error", "hash": "" };
+
     }
 
 }
 
+
+document.getElementById('inp-perLeave').addEventListener('keyup', async function() {
+
+    disResults = window.localStorage.distributions;
+    disResults = JSON.parse(disResults)
+    updateValues(disResults, document.getElementById('inp-customAmount').value)
+
+})
+
+document.getElementById('inp-customAmount').addEventListener('keyup', async function() {
+    console.log(this.value)
+    let updateIt = 1;
+    if (this.value == 0)
+        updateIt = 0
+
+    if (updateIt == 1) {
+        disResults = window.localStorage.distributions;
+        disResults = JSON.parse(disResults)
+        updateValues(disResults, this.value)
+    }
+})
 
 document.getElementById('btn-pay').addEventListener('click', async function() {
     //document.getElementById('btn-pay').disabled = true;
@@ -234,12 +284,11 @@ document.getElementById('btn-pay').addEventListener('click', async function() {
         //console.log(theValue)
         let res = await ethRequestSend(toAddress, currentAccountAddress, theValue);
         //console.log(res)
-        if (res.hash != "")
-        {
+        if (res.hash != "") {
             property = JSON.parse(window.localStorage.currentDataItem);
-            theDate= getTodatsDate();
-            let theJson ={"name":`${disResults.owners[i].name}`,"amountInternational":`${disResults.owners[i].payAmountInternational}`,"amountCrypto":`${disResults.owners[i].payAmount}`,"hash":`${res.hash}`,"paidBy":6,"propertyId":property.id,"propertyOwnerId":`${disResults.owners[i].id}`,"datePaid":theDate}
-             let bodyObj = {
+            theDate = getTodatsDate();
+            let theJson = { "name": `${disResults.owners[i].name}`, "amountInternational": `${disResults.owners[i].payAmountInternational}`, "amountCrypto": `${disResults.owners[i].payAmount}`, "hash": `${res.hash}`, "paidBy": 6, "propertyId": property.id, "propertyOwnerId": `${disResults.owners[i].id}`, "datePaid": theDate }
+            let bodyObj = {
                 table: "property_distribution",
                 tableData: theJson,
             }
