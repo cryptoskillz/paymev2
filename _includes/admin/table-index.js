88d31468c7acd5127cc9e7871a232922678b505f
chris <cryptoskillz@chriss-MacBook-Air.local>
2022-12-14 22:39:02 +0700

    clean up

--- _includes/admin/table-index.js
+++ _includes/admin/table-index.js
@@ -26,7 +26,25 @@ let propertySelectChange = (id, theElement) => {
 
 whenDocumentReady(isReady = () => {
 
+    if (typeof lookUps === 'undefined') {
+        var lookUps = "";
+    } else {
+        lookUps = JSON.stringify(lookUps);
+    }
+    console.log(lookUps);
+
     let getTableDone = (res) => {
+
+        let deleteButton = "";
+        let editButton = "";
+
+        if (typeof formatFields === 'undefined') {
+            formatFields = "";
+        }
+
+        if (typeof customSelect === 'undefined') {
+            customSelect = "";
+        }
 
         if (typeof customButton === 'undefined') {
             customButton = "";
@@ -34,7 +52,23 @@ whenDocumentReady(isReady = () => {
         if (typeof customSelect === 'undefined') {
             customSelect = "";
         }
-        
+
+        if (typeof localLookUp === 'undefined') {
+            localLookUp = "";
+        }
+
+        if (typeof localLookupField === 'undefined') {
+            localLookupField = "";
+        }
+
+        if (typeof hideEdit === 'undefined') {
+            hideEdit = "";
+        }
+
+        if (typeof hideDelete === 'undefined') {
+            hideDelete = "";
+        }
+
         // customButton = "";
         //parse json
         res = JSON.parse(res)
@@ -51,22 +85,27 @@ whenDocumentReady(isReady = () => {
             //set the data 
             let theData = res.data[i];
             //build the buttons
-            let deleteButton = "";
-            let editButton = "";
+            deleteButton = "";
+            editButton = "";
             //parse the custom button
-            customButton = customButton.replaceAll("[id]",theData.id);
-            customButton = customButton.replaceAll("[name]",theData.name);
-            customButton = customButton.replaceAll("[counter]",i);
+            customButton = customButton.replaceAll("[id]", theData.id);
+            customButton = customButton.replaceAll("[name]", theData.name);
+            customButton = customButton.replaceAll("[counter]", i);
             //parse the custom select
-            customSelect = customSelect.replaceAll("[id]",theData.id);
-            customSelect = customSelect.replaceAll("[name]",theData.name);
-            customSelect = customSelect.replaceAll("[counter]",i);
-  
+            customSelect = customSelect.replaceAll("[id]", theData.id);
+            customSelect = customSelect.replaceAll("[name]", theData.name);
+            customSelect = customSelect.replaceAll("[counter]", i);
+
 
             //check if its an admin
             if (user.isAdmin == 1) {
                 editButton = `<a href="${theCrumb}edit?id=${theData.id}" class="d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-edit fa-sm text-white-50"></i> Edit</a>`
                 deleteButton = `<a href="javascript:deleteTableItem(${theData.id},'api/database/table/','${theTable}')" class="d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-trash fa-sm text-white-50"></i> Delete</a>`
+                if (hideEdit == 1)
+                    editButton = "";
+                if (hideDelete == 1)
+                    deleteButton = "";
+
             }
 
             //set a table row array
@@ -81,17 +120,35 @@ whenDocumentReady(isReady = () => {
                 let tmpValue = tData[key];
                 //check if we have to format it
                 //note this is checking the field name we could use the scheam and check if it is real 
-                if (key == "amount")
-                    tmpValue = formatCurencyBaht(tmpValue);
-                //add the row
 
                 //check if its a hyperlink 
                 let res = isValidHttpUrl(tmpValue);
                 if (res == true) {
                     tmpValue = `<a href="${tmpValue}" target="_blank">${tmpValue}</a>`
                 }
+
+                //check if it is a local look up
+                if (key == localLookupField) {
+                    //loop through the local url
+                    for (var i2 = 0; i2 < localLookUp.length; ++i2) {
+                        let tData2 = localLookUp[i2];
+                        if (tData2.lookValue = tmpValue)
+                            tmpValue = tData2.replaceValue
+                        //console.log(tData2)
+                    }
+                }
+
+
+                for (var i2 = 0; i2 < formatFields.length; ++i2) {
+                    if (key == formatFields[i2].field) {
+                        //console.log(formatFields[i2])
+                        tmpValue = eval(formatFields[i2].function)
+                    }
+                }
+
                 tableRow.push(tmpValue);
             }
+
             buildColumn = 1;
             tableRow.push(`${editButton} ${deleteButton} ${customButton} ${customSelect} `);
             //add the table rows
@@ -101,16 +158,25 @@ whenDocumentReady(isReady = () => {
                 .node().id = theData.id;
 
         }
-        table.columns.adjust();
+        //var head_item = table.columns(1).header();
+        //console.log(head_item)
+        //document.getElementById(head_item).innerHTML = 'sss'
+        //table.column( 0 ).header().text( 'My title' ); 
+
+        if ((editButton == "") && (deleteButton == "") && (customButton == "") && (customSelect == "")) {
+            table.column(table.columns().nodes().length - 1).visible(false)
+            table.columns.adjust();
+        }
     }
 
     //get the table results for this level.
     let getTableData = () => {
 
+
         if (foreignKeys == "")
-            url = adminUrl + `database/table?checkAdmin=${checkAdmin}&tablename=${theTable}&fields=${theFields}&getOnlyTableSchema=${getOnlyTableSchema}`
+            url = adminUrl + `database/table?checkAdmin=${checkAdmin}&tablename=${theTable}&fields=${theFields}&getOnlyTableSchema=${getOnlyTableSchema}&theData=${lookUps}`
         else
-            url = adminUrl + `database/table?checkAdmin=${checkAdmin}&tablename=${theTable}&fields=${theFields}&getOnlyTableSchema=${getOnlyTableSchema}&id=${window.localStorage.currentDataItemId}&foreignId=${foreignKeys.id}`;
+            url = adminUrl + `database/table?checkAdmin=${checkAdmin}&tablename=${theTable}&fields=${theFields}&getOnlyTableSchema=${getOnlyTableSchema}&id=${window.localStorage.currentDataItemId}&foreignId=${foreignKeys.id}&lookUps=${lookUps}`;
         xhrcall(1, url, "", "json", "", getTableDone, token);
     }
 
@@ -126,7 +192,7 @@ whenDocumentReady(isReady = () => {
     //check if we have a current data item 
     if (window.localStorage.currentDataItem == "") {
         //build the json to get the main record from the main table so we can get the foreign ids.
-        url = adminUrl + `database/table?tablename=${window.localStorage.mainTable}&fields=&getOnlyTableSchema=${getOnlyTableSchema}&id=${window.localStorage.currentDataItemId}&foreignId=`
+        url = adminUrl + `database/table?tablename=${window.localStorage.mainTable}&fields=&getOnlyTableSchema=${getOnlyTableSchema}&id=${window.localStorage.currentDataItemId}&foreignId=&theData=${lookUps}`
         xhrcall(1, url, "", "json", "", getMainTableDone, token)
     } else {
         //build the json
---
+++ _includes/property-distributions-new.js
@@ -0,0 +1,337 @@
+//set up web3
+let web3 = new Web3(Web3.givenProvider || RPCUrl);
+
+let currentAccountAddress;
+let currentBalance = 0;
+let property;
+var disResults;
+
+const aggregatorV3InterfaceABI = [{
+        inputs: [],
+        name: "decimals",
+        outputs: [{ internalType: "uint8", name: "", type: "uint8" }],
+        stateMutability: "view",
+        type: "function",
+    },
+    {
+        inputs: [],
+        name: "description",
+        outputs: [{ internalType: "string", name: "", type: "string" }],
+        stateMutability: "view",
+        type: "function",
+    },
+    {
+        inputs: [{ internalType: "uint80", name: "_roundId", type: "uint80" }],
+        name: "getRoundData",
+        outputs: [
+            { internalType: "uint80", name: "roundId", type: "uint80" },
+            { internalType: "int256", name: "answer", type: "int256" },
+            { internalType: "uint256", name: "startedAt", type: "uint256" },
+            { internalType: "uint256", name: "updatedAt", type: "uint256" },
+            { internalType: "uint80", name: "answeredInRound", type: "uint80" },
+        ],
+        stateMutability: "view",
+        type: "function",
+    },
+    {
+        inputs: [],
+        name: "latestRoundData",
+        outputs: [
+            { internalType: "uint80", name: "roundId", type: "uint80" },
+            { internalType: "int256", name: "answer", type: "int256" },
+            { internalType: "uint256", name: "startedAt", type: "uint256" },
+            { internalType: "uint256", name: "updatedAt", type: "uint256" },
+            { internalType: "uint80", name: "answeredInRound", type: "uint80" },
+        ],
+        stateMutability: "view",
+        type: "function",
+    },
+    {
+        inputs: [],
+        name: "version",
+        outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+        stateMutability: "view",
+        type: "function",
+    },
+]
+
+
+//add a ready function
+let whenDocumentReady = (f) => {
+    /in/.test(document.readyState) ? setTimeout('whenDocumentReady(' + f + ')', 9) : f()
+}
+
+//this function gets the list of accounts from metamask
+const getAccounts = async () => {
+    account = "";
+    account = await web3.eth.getAccounts();
+    return account[0];
+}
+
+const updateValues = async (disResults, customValue = 0) => {
+
+
+    //get the balance from Metamask
+    let balance = await web3.eth.getBalance(currentAccountAddress);
+    //convert it to something useful
+    currentBalance = await web3.utils.fromWei(balance, 'ether');
+    //set the contract address
+    const addr = "0x2514895c72f50D8bd4B4F9b1110F0D6bD2c97526";
+    //set the price contract
+    const priceFeed = new web3.eth.Contract(aggregatorV3InterfaceABI, addr);
+    //get the latest price
+    let roundData = await priceFeed.methods.latestRoundData().call();
+    //get the decimals (didn't mention this in the docs anywhere did you)
+    let decimals = await priceFeed.methods.decimals().call();
+    //convert the stupid big number
+    let currentPrice = Number((roundData.answer.toString() / Math.pow(10, decimals)).toFixed(2));
+    //get the value of the crypto in USD
+    //note : make this the default currency from the settings
+    let cyrptoValue
+    if (customValue == 0)
+        cyrptoValue = currentPrice / 100 * currentBalance * 100
+    else
+        cyrptoValue = currentPrice / 100 * customValue * 100
+
+
+    //update the values
+    cryptoValue = Number(cyrptoValue.toFixed(2));
+    let cyrptoValuef = formatCurencyUSD(cryptoValue);
+    let totalCostsf = formatCurencyUSD(disResults.totalCosts);
+    let totalPaymentsf = formatCurencyUSD(disResults.totalPayments);
+    let totalDistf = formatCurencyUSD(disResults.totalDistributions);
+    let totalLeftf = formatCurencyUSD(disResults.totalLeft);
+    let perToLeave = document.getElementById('inp-perLeave').value;
+    let totalToPay
+    let totalToPayUSD
+    if (customValue == 0) {
+        totalToPay = currentBalance / 100 * perToLeave;
+        totalToPay = currentBalance - totalToPay
+        totalToPayUSD = cryptoValue / 100 * perToLeave
+        totalToPayUSD = cryptoValue - totalToPayUSD;
+    } else {
+        totalToPay = customValue / 100 * perToLeave;
+        totalToPay = customValue - totalToPay
+        totalToPayUSD = cryptoValue / 100 * perToLeave
+        totalToPayUSD = cryptoValue - totalToPayUSD;
+    }
+
+    if (customValue > currentBalance) {
+        updateIt = 0;
+        showAlert(`Custom amount cannot be greater than ${currentBalance}`, 2, 1)
+        updateValues(disResults)
+    } else {
+        let totalToPayUSDf = formatCurencyUSD(totalToPayUSD);
+        //console.log(totalToPay)
+        if (disResults.totalLeft > cryptoValue) {
+            showAlert("There is less funds in the account than the total owed so we are going to use the whole balance", 2, 0)
+        }
+        let table = `<table class="table">`
+        table = addTableRow("Current BNB price", formatCurencyUSD(currentPrice), table);
+        table = addTableRow("Token", `<a href="${disResults.token.blockExplorerUrl}" target="_blank">${disResults.token.name}</a>`, table);
+        table = addTableRow("Available BNB", `${currentBalance} (${cyrptoValuef})`, table);
+        table = addTableRow("Total Costs", totalCostsf, table);
+        table = addTableRow("Total Payments", totalPaymentsf, table);
+        table = addTableRow("Total Distributions", totalDistf, table);
+        table = addTableRow("Total Left", totalLeftf, table);
+        table = addTableRow("Total To Pay", ` ${totalToPay.toFixed(8)} (${totalToPayUSDf})`, table);
+        table = table + "</table>";
+        //console.log(table)
+        let theTable = document.getElementById("dist-table");
+        theTable.innerHTML = table;
+
+        table = $('#owners-table').DataTable();
+        table.clear();
+        for (var i = 0; i < disResults.owners.length; ++i) {
+            let theData = disResults.owners[i];
+            let per = theData.tokenAmount / disResults.token.totalSupply;
+            per = per * 100
+            per = Math.round(per);
+
+            //work out the amount of eth they getting
+            let ownerAmount = totalToPay / 100 * per;
+            ownerAmount = totalToPay - ownerAmount;
+
+            let ownerToPayUSD = totalToPayUSD / 100 * per
+            let ownerToPayUSDf = formatCurencyUSD(ownerToPayUSD);
+            //ownerToPayUSD = currentPrice - ownerToPayUSD;
+            disResults.owners[i].payAmount = ownerAmount;
+            disResults.owners[i].payAmountInternational = ownerToPayUSD;
+            //console.log(theData)
+
+            var rowNode = table
+                .row.add([theData.name, theData.email, theData.cryptoAddress,
+                    `${theData.tokenAmount} (${per}%)`, `${ownerAmount.toFixed(8)} (${ownerToPayUSDf})`
+                ])
+                .draw()
+
+        }
+        table.columns.adjust();
+
+        if (customValue == 0)
+            document.getElementById('inp-customAmount').value = totalToPay.toFixed(8);
+
+        window.localStorage.distributions = JSON.stringify(disResults);
+    }
+}
+
+
+//check if we are connected to metamask and if not then show the connect meta button
+const isConnected = async (disResults) => {
+    //console.log(disResults)
+    //set conneciton var
+    let conn = false;
+    //call ethrequest
+    //note this has replaced get accounts
+    currentAccountAddress = await ethRequest();
+    //check it is the same address as stored for the property.
+    let tmpPaymentAddress = property.paymentAddress.toLowerCase();
+    currentAccountAddress = currentAccountAddress.toLowerCase();
+    if (tmpPaymentAddress != currentAccountAddress) {
+        showAlert('Payment address does not match connected Metamask', 2, 0);
+        return conn;
+    } else {
+        //check we have a current account
+        if (currentAccountAddress === undefined) {
+            //no accounts show connect account button
+            showAlert('Please connect Metamask', 1, 0);
+            //set connectiont to false
+            conn = false;
+        } else {
+            updateValues(disResults)
+            document.getElementById('showBody').classList.remove('d-none');
+            document.getElementById("spinner").classList.add("d-none");
+            //document.getElementById('btn-pay').disabled = false;
+        }
+        //set connection to true
+        conn = true;
+    }
+}
+
+const ethRequest = async () => {
+    try {
+        //ask meta mask for the accounts
+        let accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+        //return them 
+        return (accounts[0])
+    } catch (e) {
+        //show the error (usually user hitting the reject button on metamask)
+        showAlert(e.message, 2);
+    }
+
+}
+
+
+const ethRequestSend = async (toAddress, fromAddress, theValue) => {
+    try {
+        const transactionParameters = {
+            from: fromAddress,
+            to: toAddress,
+            value: theValue,
+        };
+
+        // txHash is a hex string
+        // As with any RPC call, it may throw an error
+        const txHash = await ethereum.request({
+            method: 'eth_sendTransaction',
+            params: [transactionParameters],
+        });
+        return { "status": "ok", "hash": `${txHash}` };
+    } catch (e) {
+        //show the error (usually user hitting the reject button on metamask)
+        return { "status": "error", "hash": "" };
+
+    }
+
+}
+
+
+document.getElementById('inp-perLeave').addEventListener('keyup', async function() {
+    console.log(this.value);
+    if (this.value < 101) {
+        disResults = window.localStorage.distributions;
+        disResults = JSON.parse(disResults)
+        updateValues(disResults, document.getElementById('inp-customAmount').value);
+    }
+    else
+    {
+        showAlert('% to leave cannot be more than 100% come on now you know this.',2)
+    }
+})
+
+document.getElementById('inp-customAmount').addEventListener('blur', async function() {
+    console.log(this.value)
+    let updateIt = 1;
+    if (this.value == 0)
+        updateIt = 0
+
+    if (updateIt == 1) {
+        disResults = window.localStorage.distributions;
+        disResults = JSON.parse(disResults)
+        updateValues(disResults, this.value)
+    }
+})
+
+document.getElementById('btn-pay').addEventListener('click', async function() {
+    //document.getElementById('btn-pay').disabled = true;
+    currentAccountAddress = await ethRequest();
+    //console.log(currentAccountAddress)
+    disResults = window.localStorage.distributions;
+    disResults = JSON.parse(disResults)
+    for (var i = 0; i < disResults.owners.length; ++i) {
+        let toAddress = disResults.owners[i].cryptoAddress;
+        let theValue = disResults.owners[i].payAmount;
+
+        theValue = await web3.utils.toWei(String(theValue), 'ether');
+        theValue = await web3.utils.toHex(theValue)
+        //console.log(toAddress)
+        //console.log(fromAddress)
+        //console.log(theValue)
+        let res = await ethRequestSend(toAddress, currentAccountAddress, theValue);
+        //console.log(res)
+        if (res.hash != "") {
+            property = JSON.parse(window.localStorage.currentDataItem);
+            theDate = getTodatsDate();
+            let theJson = { "name": `${disResults.owners[i].name}`, "amountInternational": `${disResults.owners[i].payAmountInternational}`, "amountCrypto": `${disResults.owners[i].payAmount}`, "hash": `${res.hash}`, "paidBy": 6, "propertyId": property.id, "propertyOwnerId": `${disResults.owners[i].id}`, "datePaid": theDate }
+            let bodyObj = {
+                table: "property_distribution",
+                tableData: theJson,
+            }
+            let bodyObjectJson = JSON.stringify(bodyObj);
+
+            let xhrDone = (res) => {
+                console.log(res)
+                res = JSON.parse()
+                showAlert(res.message, 1, 0, 1);
+            }
+            //post the record
+            xhrcall(0, `api/database/table/`, bodyObjectJson, "json", "", xhrDone, token)
+        }
+    }
+    //document.getElementById('btn-pay').disabled = false;
+
+})
+
+
+
+whenDocumentReady(isReady = () => {
+
+    //process the data item.
+    let getMainTableDone = (disResults) => {
+        //store it
+        disResults = JSON.parse(disResults);
+        //console.log(disResults);
+        property = JSON.parse(window.localStorage.currentDataItem);
+        if (typeof window.ethereum !== 'undefined') {
+            //console.log('MetaMask is installed!');
+            let res = isConnected(disResults);
+
+        } else {
+            showAlert('Please connect Metamask', 2, 1);
+        }
+    }
+    document.getElementById("spinner").classList.remove("d-none");
+    showAlert('Please be patient we doing Blockchain stuff', 1)
+    url = adminUrl + `properties/distributions?id=${window.localStorage.currentDataItemId}`
+    xhrcall(1, url, "", "json", "", getMainTableDone, token);
+});
