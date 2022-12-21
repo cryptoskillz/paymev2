305904031178100e00395e31eff71732fd388359
chris <cryptoskillz@chriss-MacBook-Air.local>
2022-12-22 02:36:26 +0700

    updated price

--- functions/api/crypto/price.js
+++ functions/api/crypto/price.js
@@ -8,17 +8,24 @@ export async function onRequestGet(context) {
         next, // used for middleware or to fetch assets
         data, // arbitrary space for passing data between middlewares
     } = context;
+    let theUrl ="";
+    let theResponse;
+    let theJson;
+    let price;
     try {
         const { searchParams } = new URL(request.url);
         //get the tables cryptocurrencies 
         const cryptocurrencies = searchParams.get('cryptocurrencies');
         const fiatcurrencies = searchParams.get('fiatcurrencies');
-        let theUrl = `https://api.coingecko.com/api/v3/simple/price?ids=${cryptocurrencies}&vs_currencies=${fiatcurrencies}`
-        const theResponse = await fetch(theUrl);
-        const price = await theResponse.json();
+        theUrl = `https://api.coingecko.com/api/v3/simple/price?ids=${cryptocurrencies}&vs_currencies=${fiatcurrencies}`
+        theResponse = await fetch(theUrl);
+        price = await theResponse.json();
         return new Response(JSON.stringify(price), { status: 200 });
+        
+
+
     } catch (error) {
-        theJson = {"error":`${error}`,"url":`${theUrl}`,"response":`${theResponse}`}
+        theJson = {"url":`${theUrl}`,"response":`${price}`}
         return new Response(JSON.stringify(theJson), { status: 400 });
     }
 
