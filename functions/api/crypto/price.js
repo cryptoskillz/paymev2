83385c349537d951e7c70f0949c7a664d9a60cbb
chris <cryptoskillz@chriss-MacBook-Air.local>
2022-12-22 23:23:01 +0700

    fetching prices from chainlink

--- functions/api/crypto/price.js
+++ functions/api/crypto/price.js
@@ -37,7 +37,7 @@ export async function onRequestGet(context) {
         //note: this is a hack until i can figure out why coingecko is not working it may be limiting me for some reason on production.
         if (results == "error code: 1020")
         {
-            results = {"bitcoin":{"usd":16789.44},"ethereum":{"usd":1209.78},"bnb":{"usd":260.40}}
+            results = {"bitcoin":{"usd":16789.44},"ethereum":{"usd":1209.78},"binancecoin":{"usd":260.40}}
             results = JSON.stringify(results);
         }
         return new Response(results, { status: 200 });
