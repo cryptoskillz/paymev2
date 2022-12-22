bfb4e852161233f7a0f1dd08cd6089efea1615e6
chris <cryptoskillz@chriss-MacBook-Air.local>
2022-12-23 03:00:07 +0700

    admin

--- functions/api/crypto/address.js
+++ functions/api/crypto/address.js
@@ -1,5 +1,3 @@
-
-
 //note because --node-compat does not work on production I will have to move this to a worker until it has been resolved
 const swan = require('@swan-bitcoin/xpub-lib');
 export async function onRequestGet(context) {
@@ -17,26 +15,47 @@ export async function onRequestGet(context) {
     //get the tables
     const id = searchParams.get('id');
     const network = searchParams.get('network');
-    let theResponse ={};
-    if (id == "BTC")
-    {
-        console.log(context.env.XPUB)
-        let address =  swan.addressFromExtPubKey({ extPubKey: context.env.XPUB, network: context.env.NETWORK})
-        //console.log(address.address)
-        theResponse.address = address.address
-        theResponse.qrUrl = `https://chart.googleapis.com/chart?chs=250x250&cht=qr&chl=${theResponse.address}`
-        theResponse.path = address.path;
-        theResponse.network = context.env.NETWORK
-        //console.log(theResponse)
+    let theResponse = {};
+    try {
+        if (id == "BTC") {
+            console.log(context.env.XPUB)
+            let address = swan.addressFromExtPubKey({ extPubKey: context.env.XPUB, network: context.env.NETWORK })
+            //console.log(address.address)
+            theResponse.address = address.address
+            theResponse.qrUrl = `https://chart.googleapis.com/chart?chs=250x250&cht=qr&chl=${theResponse.address}`
+            theResponse.path = address.path;
+            theResponse.network = context.env.NETWORK
+            //console.log(theResponse)
+        } else {
+            //it is not BTC so just return the address
+            theResponse.address = context.env.CRYPTOADDRESS
+            theResponse.qrUrl = `https://chart.googleapis.com/chart?chs=250x250&cht=qr&chl=${theResponse.address}`
+            theResponse.path = "";
+        }
+        return new Response(JSON.stringify({ "data": theResponse }), { status: 200 });
+    } catch (error) {
+        //swan has died so we fall back
+        if (id == "BTC") {
+            //set the backup address to mainnet or testnet
+            if (context.env.NETWORK == "mainnet") {
+                theResponse.address = context.env.BTCBACKUPADDRESSMAIN
+            } else {
+                theResponse.address = context.env.BTCBACKUPADDRESSTEST
+            }
+            theResponse.qrUrl = `https://chart.googleapis.com/chart?chs=250x250&cht=qr&chl=${theResponse.address}`
+            theResponse.path = "";
+            theResponse.network = context.env.NETWORK
+            //console.log(theResponse)
+        } else {
+            //it is not BTC so just return the address
+            theResponse.address = context.env.CRYPTOADDRESS
+            theResponse.qrUrl = `https://chart.googleapis.com/chart?chs=250x250&cht=qr&chl=${theResponse.address}`
+            theResponse.path = "";
+        }
+        return new Response(JSON.stringify({ "data": theResponse }), { status: 200 });
+
     }
-    else
-    {
-        //it is not BTC so just return the address
-        theResponse.address = context.env.CRYPTOADDRESS
-        theResponse.qrUrl = `https://chart.googleapis.com/chart?chs=250x250&cht=qr&chl=${theResponse.address}`
-        theResponse.path = "";
-    }
-    return new Response(JSON.stringify({ "data": theResponse }), { status: 200 });
+
 }
 
 
