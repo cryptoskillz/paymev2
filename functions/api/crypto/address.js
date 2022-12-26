445798b2537bf3429b6ab6e16d42a8263f629115
chris <cryptoskillz@chriss-MacBook-Air.local>
2022-12-26 22:40:02 +0700

    added ETH and BNB payment types

--- functions/api/crypto/address.js
+++ functions/api/crypto/address.js
@@ -1,5 +1,5 @@
 //note because --node-compat does not work on production I will have to move this to a worker until it has been resolved
-
+/*
 const swan = require('@swan-bitcoin/xpub-lib');
 export async function onRequestGet(context) {
     //build the paramaters
@@ -58,9 +58,9 @@ export async function onRequestGet(context) {
     }
 
 }
+*/
 
 
-/*
 export async function onRequestGet(context) {
     //build the paramaters
     const {
@@ -102,4 +102,4 @@ export async function onRequestGet(context) {
     }
     return new Response(JSON.stringify({ "data": theResponse }), { status: 200 });
 }
-*/
+
